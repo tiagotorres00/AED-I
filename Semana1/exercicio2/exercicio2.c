cfec517613bf5cf1897ce8c6ca8e6f6773689320
Tiago Torres <00tiagotorres@gmail.com>
2023-02-13 19:13:39 -0300

    Quase pronto

--- Semana1/exercicio2/exercicio2.c
+++ Semana1/exercicio2/exercicio2.c
@@ -1,16 +1,3 @@
-/*
-Faça um programa que armazena nomes.
-O programa deve armazenar todos os nomes na mesma string.
-O tamanho da string deve crescer e diminuir conforme nomes forem colocados ou removidos. 
-Não pode ter desperdício de memória.
-
-Faça o seguinte menu:
-1) Adicionar nome
-2) Remover nome
-3) Listar
-4) Sair
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
